1d9b01c44d1233df31d38ab1c23d9d5a58933358
nithingangadharan <141124381+nithingangadharan@users.noreply.github.com>
2023-08-01 14:58:02 +0530

    Update streamlit_app.py
    Adding Header 
    Adding text under the header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new Healthy dinner')
+streamlit.header('Breakfast Menu')
+strealit.text('Omega 3 Blueberry Oatmeal')
+strealit.text('Kale,Spinach & Rocket Smootie')
+strealit.text('Hard-Boiled Free Range Egg')
+strealit.text('')
