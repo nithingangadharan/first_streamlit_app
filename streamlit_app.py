9925369e98d44170717f4c0d3663d4a71507495e
nithingangadharan <141124381+nithingangadharan@users.noreply.github.com>
2023-08-01 14:59:20 +0530

    Update streamlit_app.py
    Fix Error in text under heading

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 
 streamlit.title('My parents new Healthy dinner')
 streamlit.header('Breakfast Menu')
-strealit.text('Omega 3 Blueberry Oatmeal')
-strealit.text('Kale,Spinach & Rocket Smootie')
-strealit.text('Hard-Boiled Free Range Egg')
-strealit.text('')
+streamlit.text('Omega 3 Blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smootie')
+streamlit.text('Hard-Boiled Free Range Egg')
