bca685991849e43ddab6190b92ad69d2be392956
nithingangadharan <141124381+nithingangadharan@users.noreply.github.com>
2023-08-03 17:56:26 +0530

    Update streamlit_app.py
    Put All the Import Commands Together, At the Top

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urlib.error import URLError
 
 streamlit.title('My parents new Healthy dinner')
 streamlit.header('Breakfast Menu')
